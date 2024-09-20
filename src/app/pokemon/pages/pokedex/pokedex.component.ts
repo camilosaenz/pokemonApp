a22449a080e2abe7e8e698a1ff8b600a4cab8656
camilosaenz <48153830+camilosaenz@users.noreply.github.com>
2024-09-20 12:05:31 -0500

    Update pokedex.component.ts Sort Pokemon Id

--- src/app/pokemon/pages/pokedex/pokedex.component.ts
+++ src/app/pokemon/pages/pokedex/pokedex.component.ts
@@ -11,23 +11,28 @@ export class PokedexComponent implements OnInit {
   pokedex: Pokemon[] = [];
   termino: string | number = '';
 
-
   constructor(private pokemonService: PokemonService) {}
+
   ngOnInit(): void {
     this.pokemonService.getPokemon().subscribe(
       (resp) => {
         resp.results.forEach((result) => {
-          this.pokemonService
-            .getPokemonByName(result.name)
-            .subscribe((pokemon) => {
+          this.pokemonService.getPokemonByName(result.name).subscribe(
+            (pokemon) => {
               this.pokedex.push(pokemon);
-            });
+              this.sortPokedexById();
+            }
+          );
         });
       },
       (err) => {
         console.log(err);
       }
     );
+  }
+
+  sortPokedexById() {
+    this.pokedex.sort((a, b) => a.id - b.id);
   }
 
   getType(list: any[]) {
@@ -39,6 +44,7 @@ export class PokedexComponent implements OnInit {
     this.pokemonService.getPokemonByNameOrId(termino).subscribe(
       (pokemon) => {
         this.pokedex = pokemon;
+        this.sortPokedexById();
       },
       (err) => {
         this.pokedex = [];
